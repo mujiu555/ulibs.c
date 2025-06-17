4a9f3b5d93c17d6a7ef85209b4d4c1ff76abb28e
dot <1310769510@qq.com>
2025-06-17 17:53:18 +0800

    fix: fix some minor bug of each macro
    fix:
    1. inc/macros.h: add parentheses around the body of `SWAP` macro

--- inc/macros.h
+++ inc/macros.h
@@ -8,18 +8,20 @@
 
 #define RANGES(T, var, from, to, skip)                                         \
   for (T v##from##__from = (from), v##to##__to = (to), v##var##__keep = true;  \
-       v##var##__keep; v##var##__keep = !v##var##__keep)                       \
+       v##var##__keep;                                                         \
+       v##var##__keep = !v##var##__keep)                                       \
     for (T var = v##from##__from;                                              \
          v##from##__from < v##to##__to ? var < v##to##__to                     \
                                        : var > v##to##__to;                    \
          var += skip)
 
 #define FOREACH(T, var, arr, len)                                              \
-  for (size_t v##var##__keep = true, v##var##__count = 0,                      \
-              v##var##_len = (len);                                            \
+  for (size_t v##var##__keep  = true,                                          \
+              v##var##__count = 0,                                             \
+              v##var##_len    = (len);                                         \
        v##var##__keep && v##var##__count < v##var##_len;                       \
        v##var##__keep = !v##var##__keep, v##var##__count++)                    \
-    for (T *var = (T *)(arr) + v##var##__count; v##var##__keep;                \
+    for (T *var         = (T *)(arr) + v##var##__count; v##var##__keep;        \
          v##var##__keep = !v##var##__keep)
 
 #define ARRLEN(arr) (sizeof(arr) / sizeof(*(arr)))
@@ -41,10 +43,10 @@
   })
 
 #define SWAP(x, y)                                                             \
-  {                                                                            \
+  ({                                                                           \
     (void)(&x == &y);                                                          \
     typeof(x) __tmp = x;                                                       \
-    x = y;                                                                     \
-    y = __tmp;                                                                 \
-  }
+    x               = y;                                                       \
+    y               = __tmp;                                                   \
+  })
 #endif // !ULIB__MACROS_H__
